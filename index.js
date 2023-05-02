90f9823c51e9ea1bd251faf2a94d5bbe5c8ff8d4
iyaki <ivanyaki96@gmail.com>
2023-05-02 09:00:13 -0300

    Agrego blackestList

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 import whiteList from './data/whiteList.json' assert {type: 'json'}
 import blackList from './data/blackList.json' assert {type: 'json'}
+import blackestList from './data/blackestList.json' assert {type: 'json'}
 
 const baseUrl = 'https://cloud.feedly.com'
 const wordSeparatorRegex = /[\s.,;:!?\-]+/g
 
-const streamsParams = (new URLSearchParams({
-	streamId: process.env.COLLECTION,
-	count: 250,
-	unreadOnly: true
-})).toString()
-const unreadEntries = await (await fetch(
-	`${baseUrl}/v3/streams/contents?${streamsParams}`,
-	{
-		method: 'GET',
-		headers: {
-			Authorization: `Bearer ${process.env.CLIENT_SECRET}`
-		}
-	}
-)).json()
+let continuation = null
+let unreadEntries = []
+while (true) {
+	const streamsParams = (new URLSearchParams({
+		streamId: process.env.COLLECTION,
+		count: 250,
+		ranked: 'oldest',
+		continuation: continuation,
+		unreadOnly: true
+	})).toString()
 
-const entriesToMarkAsRead = unreadEntries.items.filter(filterEntry)
+	const response = await (await fetch(
+		`${baseUrl}/v3/streams/contents?${streamsParams}`,
+		{
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer ${process.env.CLIENT_SECRET}`
+			}
+		}
+	)).json()
+
+	unreadEntries = unreadEntries.concat(response.items)
+
+	if (typeof response.continuation === 'undefined') {
+		break
+	}
+	continuation = response.continuation
+
+}
+
+const entriesToMarkAsRead = unreadEntries.filter(filterEntry)
 
 function filterEntry(entry) {
 	return filterTitle(entry) || filterContent(entry)
@@ -27,8 +43,13 @@ function filterEntry(entry) {
 
 function filterTitle(entry) {
 	const title = entry.title.toLowerCase()
-	return filterStringTokens(title, blackList).length > 0 &&
-		filterStringTokens(title, whiteList).length === 0
+	return (
+		filterStringTokens(title, blackestList).length > 0 ||
+		(
+			filterStringTokens(title, blackList).length > 0 &&
+			filterStringTokens(title, whiteList).length === 0
+		)
+	)
 }
 
 function filterContent(entry) {
@@ -36,8 +57,13 @@ function filterContent(entry) {
 		.trim()
 		.toLowerCase()
 
-	return filterStringTokens(content, blackList).length > 0 &&
-		filterStringTokens(content, whiteList).length === 0
+	return (
+		filterStringTokens(content, blackestList).length > 0 ||
+		(
+			filterStringTokens(content, blackList).length > 0 &&
+			filterStringTokens(content, whiteList).length === 0
+		)
+	)
 }
 
 function filterStringTokens(string, checkTokens) {
@@ -68,6 +94,6 @@ await fetch(
 	}
 )
 
-console.log(entriesToMarkAsRead.map(entry => ({title: entry.title, content: entry.content?.content || entry.summary?.content})))
+console.log(entriesToMarkAsRead.map(entry => entry.title + ' - ' + entry.canonicalUrl))
 console.log('----------------------------------------------------------------------------------')
 console.log(`${entriesToMarkAsRead.length} entries marked as read`)
